4e5f9bc41ba4c7cd61971534ecdeb5f39924d090
baekdoeun <donna8780@naver.com>
2024-12-17 12:26:57 +0900

    게시글 생성 중 제목 및 내용에 대한 유효성 검증 기능 추가

--- src/main/java/com/pawstime/pawstime/domain/post/dto/req/CreatePostReqDto.java
+++ src/main/java/com/pawstime/pawstime/domain/post/dto/req/CreatePostReqDto.java
@@ -4,10 +4,16 @@ import com.pawstime.pawstime.domain.board.entity.Board;
 import com.pawstime.pawstime.domain.post.entity.Post;
 import com.pawstime.pawstime.domain.post.entity.Post.PostCategory;
 import com.pawstime.pawstime.domain.user.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 
 public record CreatePostReqDto(
+    @NotBlank(message = "제목은 필수 입력값입니다.")
+    @Size(min = 5, max = 20, message = "제목은 5자 이상, 20자 이하로 작성해야 합니다.")
     String title,           // 제목
+    @NotBlank(message = "내용은 필수 입력값입니다.")
+    @Size(min = 5, message = "내용은 최소 5자 이상이어야 합니다.")
     String content,         // 내용
     Long userId,            // 작성자 ID
     Long boardId,           // 게시판 ID
