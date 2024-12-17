ffe4bbe56e65e6a76fb32e9bbda76f5bf38ccec1
baekdoeun <donna8780@naver.com>
2024-12-17 12:26:57 +0900

    게시글 생성 중 제목 및 내용에 대한 유효성 검증 기능 추가

--- src/main/java/com/pawstime/pawstime/domain/post/controller/PostController.java
+++ src/main/java/com/pawstime/pawstime/domain/post/controller/PostController.java
@@ -4,9 +4,11 @@ import com.pawstime.pawstime.domain.post.dto.req.CreatePostReqDto;
 import com.pawstime.pawstime.domain.post.facade.PostFacade;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +24,17 @@ public class PostController {
   private final PostFacade postFacade;
   @Operation(summary = "게시글 생성", description = "새로운 게시글을 생성할 수 있습니다.")
   @PostMapping("/posts")
-  public ResponseEntity<String> createPost(@RequestBody CreatePostReqDto req){
+  public ResponseEntity<String> createPost(@RequestBody @Valid CreatePostReqDto req, BindingResult bindingResult){
+    // 유효성 검사 오류가 있을 경우 처리
+    if (bindingResult.hasErrors()) {
+      StringBuilder errorMessages = new StringBuilder();
+      bindingResult.getAllErrors().forEach(error ->
+          errorMessages.append(error.getDefaultMessage()).append("\n")
+      );
+      return ResponseEntity.badRequest().body("유효성 검사 실패: \n" + errorMessages.toString());
+    }
     try{
       postFacade.createPost(req);
-
       return ResponseEntity.ok().body("게시글 생성이 완료되었습니다.");
     }catch (Exception e){
       return ResponseEntity.badRequest().body("게시글 생성 중 오류가 발생했습니다. "+ e.getMessage());
