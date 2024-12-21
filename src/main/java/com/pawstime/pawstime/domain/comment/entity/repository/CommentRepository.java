b247af13388149179040d42c3fca6fce670e4ec0
dgf0020 <tmxlcl0020@naver.com>
2024-12-22 00:09:00 +0900

    feat: 댓글 전체 목록 조회 기능 구현

--- src/main/java/com/pawstime/pawstime/domain/comment/entity/repository/CommentRepository.java
+++ src/main/java/com/pawstime/pawstime/domain/comment/entity/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.pawstime.pawstime.domain.comment.entity.repository;
 
 import com.pawstime.pawstime.domain.comment.entity.Comment;
 import com.pawstime.pawstime.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
   @Query("SELECT p FROM Post p WHERE p.postId = :postId AND p.isDelete = false")
   Post findByIdQuery(Long postId);
+
+  @Query("SELECT c FROM Comment c WHERE c.isDelete = false")
+  Page<Comment> findAllQuery(Pageable pageable);
 }
--- src/main/java/com/pawstime/pawstime/domain/comment/service/ReadCommentService.java
+++ src/main/java/com/pawstime/pawstime/domain/comment/service/ReadCommentService.java
@@ -1,8 +1,12 @@
 package com.pawstime.pawstime.domain.comment.service;
 
+import com.pawstime.pawstime.domain.comment.entity.Comment;
 import com.pawstime.pawstime.domain.comment.entity.repository.CommentRepository;
 import com.pawstime.pawstime.domain.post.entity.Post;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +17,9 @@ public class ReadCommentService {
 
   public Post findByIdQuery(Long postId) {
     return commentRepository.findByIdQuery(postId);
+  }
+
+  public Page<Comment> getCommentAll(Pageable pageable) {
+    return commentRepository.findAllQuery(pageable);
   }
 }
