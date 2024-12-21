90eafb97ac12c49e7782dc6842025c54ea904923
baekdoeun <donna8780@naver.com>
2024-12-21 18:27:00 +0900

    feat/조회수 증가 기능 추ì가

--- src/main/java/com/pawstime/pawstime/domain/post/service/GetDetailPostService.java
+++ src/main/java/com/pawstime/pawstime/domain/post/service/GetDetailPostService.java
@@ -13,7 +13,10 @@ public class GetDetailPostService {
 
     public GetDetailPostRespDto getDetailPost(Long postId){
         Post post = postRepository.findById(postId)
-                .orElse(null);;
+                .orElse(null);
+        // 조회수 증가
+        post.increaseViews(); // 조회수 증가
+        postRepository.save(post); // 조회수가 증가된 게시글 저장
 
         return GetDetailPostRespDto.from(post);  // DTO 반환
 
