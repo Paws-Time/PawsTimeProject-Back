fd32fbab49ca5219e480a32baf43934054050df2
dgf0020 <tmxlcl0020@naver.com>
2024-12-22 14:49:42 +0900

    feat: 댓글 삭제 기능 구현

--- src/main/java/com/pawstime/pawstime/domain/comment/controller/CommentController.java
+++ src/main/java/com/pawstime/pawstime/domain/comment/controller/CommentController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -100,6 +101,28 @@ public class CommentController {
     } catch (Exception e) {
       return ApiResponse.generateResp(
           Status.ERROR, "게시글 댓글 조회 중 오류가 발생하였습니다 : " + e.getMessage(), null);
+    }
+  }
+
+  @Operation(summary = "댓글 삭제", description = "선택한 댓글을 삭제합니다.")
+  @PutMapping("/{commentId}")
+  public ResponseEntity<ApiResponse<Void>> deleteComment(@PathVariable Long commentId) {
+    try {
+
+      commentFacade.deleteComment(commentId);
+
+      return ApiResponse.generateResp(Status.DELETE, "댓글이 삭제되었습니다.", null);
+
+    } catch (CustomException e) {
+      Status status = Status.valueOf(e.getClass()
+          .getSimpleName()
+          .replace("Exception", "")
+          .toUpperCase());
+      return ApiResponse.generateResp(status, e.getMessage(), null);
+
+    } catch (Exception e) {
+      return ApiResponse.generateResp(
+          Status.ERROR, "댓글 삭제 중 오류가 발생하였습니다 : " + e.getMessage(), null);
     }
   }
 }
