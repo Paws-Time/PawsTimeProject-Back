1fe653cee6f68412916014adf00dfe34f1b560e8
dgf0020 <tmxlcl0020@naver.com>
2024-12-22 12:03:17 +0900

    feat: 게시판 조회에서 삭제된 게시글 제외

--- src/main/java/com/pawstime/pawstime/domain/board/entity/repository/BoardRepository.java
+++ src/main/java/com/pawstime/pawstime/domain/board/entity/repository/BoardRepository.java
@@ -11,7 +11,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-  @Query("SELECT b FROM Board b WHERE b.title = :title and b.isDelete = false")
+  @Query("SELECT b FROM Board b WHERE b.title = :title AND b.isDelete = false")
   Board findByTitleQuery(String title);
 
+  @Query("SELECT b FROM Board b WHERE b.boardId = :boardId AND b.isDelete = false")
+  Board findByIdQuery(Long boardId);
+
+  @Query("SELECT b FROM Board b WHERE b.isDelete = false")
+  Page<Board> findAllQuery(Pageable pageable);
 }
