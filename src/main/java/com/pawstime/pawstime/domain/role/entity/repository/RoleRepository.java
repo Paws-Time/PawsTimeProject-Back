fa2b3d9a5d9480a11093b1dc0d6b0abee409cf90
dgf0020 <tmxlcl0020@naver.com>
2024-12-27 00:01:15 +0900

    feat: jwt를 활용한 유저기능 초기 세팅

--- src/main/java/com/pawstime/pawstime/domain/role/entity/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pawstime.pawstime.domain.role.entity.repository;
-
-import com.pawstime.pawstime.domain.role.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
