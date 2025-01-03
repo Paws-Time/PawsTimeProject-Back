d593e15130576000bbcc397eb3196b6d14ea1caa
baekdoeun <donna8780@naver.com>
2025-01-03 19:16:48 +0900

    게시판 목록 조회 시 Board_Type 추가

--- src/main/java/com/pawstime/pawstime/domain/board/dto/resp/GetBoardRespDto.java
+++ src/main/java/com/pawstime/pawstime/domain/board/dto/resp/GetBoardRespDto.java
@@ -1,27 +1,33 @@
 package com.pawstime.pawstime.domain.board.dto.resp;
 
 import com.pawstime.pawstime.domain.board.entity.Board;
+
 import java.time.LocalDateTime;
+
+import com.pawstime.pawstime.domain.board.enums.BoardType;
 import lombok.Builder;
 
 @Builder
 public record GetBoardRespDto(
-    Long boardId,
-    String title,
-    String description,
-    boolean isDelete,
-    LocalDateTime createdAt,
-    LocalDateTime updatedAt
+        Long boardId,
+        String title,
+        String description,
+        boolean isDelete,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        BoardType boardType
+
 ) {
 
-  public static GetBoardRespDto from(Board board) {
-    return GetBoardRespDto.builder()
-        .boardId(board.getBoardId())
-        .title(board.getTitle())
-        .description(board.getDescription())
-        .isDelete(board.isDelete())
-        .createdAt(board.getCreatedAt())
-        .updatedAt(board.getUpdatedAt())
-        .build();
-  }
+    public static GetBoardRespDto from(Board board) {
+        return GetBoardRespDto.builder()
+                .boardId(board.getBoardId())
+                .title(board.getTitle())
+                .description(board.getDescription())
+                .isDelete(board.isDelete())
+                .createdAt(board.getCreatedAt())
+                .updatedAt(board.getUpdatedAt())
+                .boardType(board.getBoardType())
+                .build();
+    }
 }
