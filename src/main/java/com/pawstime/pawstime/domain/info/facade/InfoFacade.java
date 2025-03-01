9e6933f440034e87fb7503c40a0e8baf56d5f7da
dgf0020 <tmxlcl0020@naver.com>
2025-03-01 11:28:40 +0900

    feat: 보호소 정보 엔티티 추가 및 지역별 조회 기능 구현

--- src/main/java/com/pawstime/pawstime/domain/info/facade/InfoFacade.java
+++ src/main/java/com/pawstime/pawstime/domain/info/facade/InfoFacade.java
@@ -1,7 +1,13 @@
 package com.pawstime.pawstime.domain.info.facade;
 
 import com.pawstime.pawstime.domain.info.dto.resp.GetHospitalInfoRespDto;
+import com.pawstime.pawstime.domain.info.dto.resp.GetShelterInfoRespDto;
+import com.pawstime.pawstime.domain.info.dto.resp.GetShelterOperationInfoRespDto;
+import com.pawstime.pawstime.domain.info.entity.ShelterOperationInfo;
 import com.pawstime.pawstime.domain.info.service.ReadHospitalInfoService;
+import com.pawstime.pawstime.domain.info.service.ReadShelterInfoService;
+import com.pawstime.pawstime.global.exception.NotFoundException;
+import java.net.ContentHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -18,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class InfoFacade {
 
     private final ReadHospitalInfoService readHospitalInfoService;
+    private final ReadShelterInfoService readShelterInfoService;
 
     public Page<GetHospitalInfoRespDto> readHospitalInfo(int pageNo, int pageSize, String sortBy, String direcrion, int addNum) {
 
@@ -25,4 +32,11 @@ public class InfoFacade {
 
         return readHospitalInfoService.readAllHospital(pageable, addNum).map(GetHospitalInfoRespDto::from);
     }
+
+  public Page<GetShelterInfoRespDto> readShelterInfo(int pageNo, int pageSize, String sortBy, String direction, int addNum) {
+
+      Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(Sort.Direction.fromString(direction), sortBy));
+
+      return readShelterInfoService.readAllShelter(pageable, addNum).map(GetShelterInfoRespDto::from);
+  }
 }
