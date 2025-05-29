b9c362aed2592617272d79ac28460325fceead14
dgf0020 <tmxlcl0020@naver.com>
2025-05-29 22:32:08 +0900

    update db url

--- src/main/java/com/pawstime/pawstime/domain/profileImg/entity/repository/ProfileRepository.java
+++ src/main/java/com/pawstime/pawstime/domain/profileImg/entity/repository/ProfileRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<ProfileImg, Long> {
 
     @Query("SELECT p FROM ProfileImg p WHERE p.user.userId = :userId")
